34ff550434fd639d24c81a40a075b3cc5bddfe93
Dayana Mariano <dayanaraquelle@gmail.com>
2024-01-29 17:13:45 -0300

    :recycle: refactor: Calcular consumindo localStorage

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const elementoMulheres = document.getElementById("elementoMulheres")
 const elementoCriancas = document.getElementById("elementoCriancas")
 const elementoBebem = document.getElementById("elementoBebem")
 
-// 
-elementoHomens.innerText = localStorage.getItem("homens") || "0"
-elementoMulheres.innerText = localStorage.getItem("mulheres") || "0"
-elementoCriancas.innerText = localStorage.getItem("criancas") || "0"
-elementoBebem.innerText = localStorage.getItem("bebem") || "0"
+// Valores salvos
+const valueHomens = localStorage.getItem("homens")
+const valueMulheres = localStorage.getItem("mulheres")
+const valueCriancas = localStorage.getItem("criancas")
+const valueBebem = localStorage.getItem("bebem")
+// Mantem nos input
+elementoHomens.innerText = valueHomens || "0"
+elementoMulheres.innerText = valueMulheres || "0"
+elementoCriancas.innerText = valueCriancas || "0"
+elementoBebem.innerText = valueBebem || "0"
 
 // Adicionar,remover e guardar no locarlStorage
 function adicionar(elemento, pessoa) {
@@ -31,6 +36,10 @@ const buttonCalcular = document.getElementById("buttonCalcular")
 const sectionCalcular = document.getElementById("sectionCalcular")
 const sectionFormulario = document.getElementById("sectionFormulario")
 buttonCalcular.addEventListener("click", calcular)
+const resultadoConvidados = document.getElementById("resultadoConvidados")
+const resultadoHomens = document.getElementById("resultadoHomens")
+const resultadoMulheres = document.getElementById("resultadoMulheres")
+const resultadoCriancas = document.getElementById("resultadoCriancas")
 const tdCarne = document.getElementById("tdCarne")
 const tdPaodeAlho = document.getElementById("tdPaodeAlho")
 const tdRefrigerante = document.getElementById("tdRefrigerante")
@@ -38,27 +47,28 @@ const tdAgua = document.getElementById("tdAgua")
 const tdCerveja = document.getElementById("tdCerveja")
 const tdCarvao = document.getElementById("tdCarvao")
 
+
 function calcular() {
   sectionCalcular.classList.add("ocultar")
   sectionFormulario.classList.remove("ocultar")
   // if(localStorage.getItem("nome")){
   //   sectionFormulario.classList.remove("ocultar")
   // }
-  const carne = (elementoHomens.textContent * 0.4) + (elementoMulheres.textContent * 0.32) + (elementoCriancas.textContent * 0.20)
+  const carne = (valueHomens * 0.4) + (valueMulheres * 0.32) + (valueCriancas * 0.20)
   console.log("carne:", carne)
   tdCarne.innerText= `${carne} kg`
 
-  const paoDeAlho = (elementoHomens.textContent * 2) + (elementoMulheres.textContent * 2) + (elementoCriancas.textContent * 1)
+  const paoDeAlho = (valueHomens * 2) + (valueMulheres * 2) + (valueCriancas * 1)
   console.log("pão de alho:", paoDeAlho)
   tdPaodeAlho.innerText = `${paoDeAlho} unidades`
   
-  const carvao = (elementoHomens.textContent * 1) + (elementoMulheres.textContent * 1) + (elementoCriancas.textContent * 1)
+  const carvao = (valueHomens * 1) + (valueMulheres * 1) + (valueCriancas * 1)
   console.log("carvão:", carvao)
   tdCarvao.innerText = `${carvao} kg`
 
 
   const calcularRefrigerante = () => {
-    const totalPessoas = (elementoHomens.textContent * 1) + (elementoMulheres.textContent * 1) + (elementoCriancas.textContent * 1)
+    const totalPessoas = (valueHomens * 1) + (valueMulheres * 1) + (valueCriancas * 1)
     const refrigerante = totalPessoas / 5
     return Math.ceil(refrigerante)
   }
@@ -66,14 +76,14 @@ function calcular() {
   tdRefrigerante.innerText = `${calcularRefrigerante()} garrafa de 2L`
 
   const calcularAgua = () => {
-    const totalPessoas = (elementoHomens.textContent * 1) + (elementoMulheres.textContent * 1) + (elementoCriancas.textContent * 1)
+    const totalPessoas = (valueHomens * 1) + (valueMulheres * 1) + (valueCriancas * 1)
     const agua = totalPessoas / 5
     return Math.ceil(agua)
   }
   console.log("agua:", calcularAgua())
   tdAgua.innerText = `${calcularAgua()} garrafa de 1L`
 
-  const cerveja = (elementoBebem.textContent * 3)
+  const cerveja = (valueBebem * 3)
   console.log("cerveja:", cerveja)
   tdCerveja.innerText = `${cerveja} garrafa de 600ml`
 
