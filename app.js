88c6d68fc0e86aa0998e35da16d2ef31690cb8df
Dayana Mariano <dayanaraquelle@gmail.com>
2024-01-29 15:54:31 -0300

    :sparkles: feat: Tabela resultado

--- app.js
+++ app.js
@@ -31,21 +31,30 @@ const buttonCalcular = document.getElementById("buttonCalcular")
 const sectionCalcular = document.getElementById("sectionCalcular")
 const sectionFormulario = document.getElementById("sectionFormulario")
 buttonCalcular.addEventListener("click", calcular)
+const tdCarne = document.getElementById("tdCarne")
+const tdPaodeAlho = document.getElementById("tdPaodeAlho")
+const tdRefrigerante = document.getElementById("tdRefrigerante")
+const tdAgua = document.getElementById("tdAgua")
+const tdCerveja = document.getElementById("tdCerveja")
+const tdCarvao = document.getElementById("tdCarvao")
 
 function calcular() {
-  // sectionCalcular.setAttribute("style","display:none;")
   sectionCalcular.classList.add("ocultar")
+  sectionFormulario.classList.remove("ocultar")
   // if(localStorage.getItem("nome")){
   //   sectionFormulario.classList.remove("ocultar")
   // }
   const carne = (elementoHomens.textContent * 0.4) + (elementoMulheres.textContent * 0.32) + (elementoCriancas.textContent * 0.20)
   console.log("carne:", carne)
+  tdCarne.innerText= `${carne} kg`
 
   const paoDeAlho = (elementoHomens.textContent * 2) + (elementoMulheres.textContent * 2) + (elementoCriancas.textContent * 1)
   console.log("pão de alho:", paoDeAlho)
+  tdPaodeAlho.innerText = `${paoDeAlho} unidades`
   
   const carvao = (elementoHomens.textContent * 1) + (elementoMulheres.textContent * 1) + (elementoCriancas.textContent * 1)
   console.log("carvão:", carvao)
+  tdCarvao.innerText = `${carvao} kg`
 
 
   const calcularRefrigerante = () => {
@@ -54,6 +63,7 @@ function calcular() {
     return Math.ceil(refrigerante)
   }
   console.log("refrigerante:", calcularRefrigerante())
+  tdRefrigerante.innerText = `${calcularRefrigerante()} garrafa de 2L`
 
   const calcularAgua = () => {
     const totalPessoas = (elementoHomens.textContent * 1) + (elementoMulheres.textContent * 1) + (elementoCriancas.textContent * 1)
@@ -61,12 +71,15 @@ function calcular() {
     return Math.ceil(agua)
   }
   console.log("agua:", calcularAgua())
+  tdAgua.innerText = `${calcularAgua()} garrafa de 1L`
 
   const cerveja = (elementoBebem.textContent * 3)
   console.log("cerveja:", cerveja)
+  tdCerveja.innerText = `${cerveja} garrafa de 600ml`
 
 }
-//C adastrar
+//Cadastrar
+const sectionTabela = document.getElementById("sectionTabela")
 const nome = document.getElementById("nome")
 const email = document.getElementById("email")
 const cep = document.getElementById("cep")
@@ -74,8 +87,13 @@ const checkComunicacoes = document.getElementById("checkComunicacoes")
 const buttonCadastrar = document.getElementById("buttonCadastrar")
 buttonCadastrar.addEventListener("click", cadastrar)
 
-function cadastrar() {
-  console.log(nome)
+function cadastrar(event) {
+  event.preventDefault()
+
+  sectionCalcular.classList.add("ocultar")
+  sectionFormulario.classList.add("ocultar")
+  sectionTabela.classList.remove("ocultar")
+
   localStorage.setItem("nome:", nome.value)
   localStorage.setItem("email:", email.value)
   localStorage.setItem("cep:", cep.value)
